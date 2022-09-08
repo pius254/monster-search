86aa0b8f32e5400a4629594f6eea2904542f215e
Pius Muriithi <piusmuriithi1@gmail.com>
2022-09-08 20:25:46 +0300

    Fetch data using useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/navbar.component";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  const getUsers = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    setUser(data);
+  };
   return (
     <div className="App">
       <Navbar />
+      {user.map((user) => (
+        <ul key={user.id}>
+          <li>{user.name}</li>
+        </ul>
+      ))}
     </div>
   );
 }
